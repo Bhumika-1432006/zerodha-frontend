89d4f09d89ce275274223518a8a3c57d2284a90f
Bhumika <guravbhumika808@gmail.com>
2026-04-02 10:22:20 +0530

    mobile app

--- src/landing_page/about/Hero.js
+++ src/landing_page/about/Hero.js
@@ -1,46 +1,44 @@
 import React from "react";
+
 function Hero() {
   return (
     <div className="container">
-      <div className="row p-5 mt-5 mb-5">
+      {/* Heading Section */}
+      <div className="row p-3 p-md-5 mt-5 mb-5">
         <h2 className="text-center fs-2">
           We pioneered the discount broking model in India.
-          <br />
+          <br className="d-none d-md-block" />
           Now, we are breaking ground with our technology.
         </h2>
       </div>
 
-      <div className="row p-5 mt-5 mb-5 border-top text-muted fs-7"
-      style={{ lineHeight: "1.9"}}
-      >
-        <div className="col p-5">
+      {/* Content Section: col-12 makes them stack vertically on mobile */}
+      <div className="row p-3 p-md-5 border-top text-muted" style={{ lineHeight: "1.9", fontSize: "1rem" }}>
+        <div className="col-12 col-md-6 mb-4 mb-md-0">
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
-            of breaking all barriers that traders and investors face in India in
-            terms of cost, support, and technology. We named the company
-            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
-            barrier.
-        </p>
-        <p>
-             Today, our disruptive pricing models and in-house
+            of breaking all barriers that traders and investors face in India. 
+            We named the company Zerodha, a combination of Zero and "Rodha", 
+            the Sanskrit word for barrier.
+          </p>
+          <p>
+            Today, our disruptive pricing models and in-house
             technology have made us the biggest stock broker in India.
-        </p>
-        <p>
-            Over 1.6+
-            crore clients place billions of orders every year through our
-            powerful ecosystem of investment platforms, contributing over 15% of
-            all Indian retail trading volumes.
+          </p>
+          <p>
+            Over 1.6+ crore clients place billions of orders every year through our
+            powerful ecosystem of investment platforms.
           </p>
         </div>
-        <div className="col p-5">
-            <p>In addition, we run a number of     popular open online educational and community initiatives to empower retail traders and investors.
-            </p>
-            <p>
-           <a href='' style={{ textDecoration: "none"}}> Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal          of growing the Indian capital markets.
-            </p>
-            <p>
-            And yet, we are always up to something new every day. Catch up on the latest updates on our blog or see what the media is  <a href=''  style={{ textDecoration: "none"}}> saying about us </a>or learn more about our business and product <a href=""  style={{ textDecoration: "none"}}>philosophies.</a>
-            </p>
+        
+        <div className="col-12 col-md-6">
+          <p>In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.</p>
+          <p>
+            <a href='' style={{ textDecoration: "none" }}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups.
+          </p>
+          <p>
+            Catch up on the latest updates on our blog or see what the media is <a href='' style={{ textDecoration: "none" }}>saying about us</a>.
+          </p>
         </div>
       </div>
     </div>
--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -1,36 +1,41 @@
 import React from 'react';
+
 function Team() {
-    return ( 
-        <div className="container">
-      <div className="row p-5 mt-5 mb-5  border-top ">
-        <h2 className="text-center ">
-         People
-        </h2>
+  return (
+    <div className="container">
+      <div className="row p-3 p-md-5 mt-5 mb-0 border-top">
+        <h2 className="text-center">People</h2>
       </div>
 
-      <div className="row p-5 mt-5 mb-5 text-muted fs-7"
-      style={{ lineHeight: "1.9"}}
-      >
-        <div className="col p-5 text-center ">
-        <img src="media/images/nithinKamath.jpg" style={{borderRadius:"100%" , width : "50%"}}></img>
-        <h4 className="mt-5">Nithin Kamath</h4>
-        <h6>Founder, CEO</h6>
+      <div className="row p-3 p-md-5 text-muted" style={{ lineHeight: "1.9" }}>
+        {/* Image Column: Full width on mobile, half on desktop */}
+        <div className="col-12 col-md-6 text-center mb-4 mb-md-0">
+          <img 
+            src="media/images/nithinKamath.jpg" 
+            alt="Nithin Kamath"
+            style={{ borderRadius: "100%", width: "60%", maxWidth: "250px" }} 
+          />
+          <h4 className="mt-4">Nithin Kamath</h4>
+          <h6>Founder, CEO</h6>
         </div>
-        <div className="col p-5">
-            <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.<br/>
-                He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).<br/>
-                Playing basketball is his zen.
-            </p>
-            <p> Connect on 
-           <a href='' style={{ textDecoration: "none"}}> Homepage </a> / <a href='' style={{ textDecoration: "none"}}> TradingQnA </a> / <a href='' style={{ textDecoration: "none"}}> Twitter</a>
-            </p>
-            <p>
-            And yet, we are always up to something new every day. Catch up on the latest updates on our blog or see what the media is  <a href=''  style={{ textDecoration: "none"}}> saying about us </a>or learn more about our business and product <a href=""  style={{ textDecoration: "none"}}>philosophies.</a>
-            </p>
+
+        {/* Bio Column: Full width on mobile, half on desktop */}
+        <div className="col-12 col-md-6">
+          <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader.
+          </p>
+          <p>
+            He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p>
+            Connect on: 
+            <a href='' className="mx-1" style={{ textDecoration: "none" }}>Homepage</a> / 
+            <a href='' className="mx-1" style={{ textDecoration: "none" }}>Twitter</a>
+          </p>
         </div>
       </div>
     </div>
-     );
+  );
 }
 
 export default Team;
