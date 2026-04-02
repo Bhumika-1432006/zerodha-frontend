865c943b0f1796db7150cc71e17ea6a1b85119f4
Bhumika <guravbhumika808@gmail.com>
2026-04-02 11:04:02 +0530

    mobile app

--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -1,47 +1,49 @@
 import React from 'react';
+
 function LeftSection({
-imageURL,
-productName,
-productDescription,
-tryDemo,
-learnMore,
-googlePlay="https://play.google.com/store/apps/details?id=com.zerodha.kite3&hl=en_IN",
-appStore,
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  // Setting default values for the store links
+  googlePlay = "https://play.google.com/store/apps/details?id=com.zerodha.kite3&hl=en_IN",
+  appStore = "https://apps.apple.com/lk/app/zerodha-kite-trade-invest/id1449453802",
 }) {
     return ( 
       <div className='container border-bottom mb-7'>
-        <div className='row '>
+        <div className='row align-items-center'>
             <div className='col-6 p-5'>
-                <img src={imageURL}  alt=""></img>
+                <img src={imageURL} alt={productName} style={{ width: "100%" }} />
             </div>
 
             <div className='col-1'>
-            {/* this is for the padding in between the two cols */}
+            {/* spacer column */}
             </div>
-            <div className='col-5 p-5  text-muted' style={{ lineHeight: "1.9"}}>
+
+            <div className='col-5 p-5 text-muted' style={{ lineHeight: "1.9"}}>
                 <h1>{productName}</h1>
                 <p>{productDescription}</p>
             
-                <div>
-                    <a href={"http://localhost:3001"} style={{textDecoration:"none"}}>Try Demo →</a>
-                    <a href={learnMore} style={{marginLeft:"50px" , textDecoration:"none"}}>Learn More →</a>
-                    
+                {/* Demo and Learn More Links (if applicable) */}
+                <div className='mb-3'>
+                    {tryDemo && <a href={tryDemo} style={{textDecoration:"none"}}>Try Demo →</a>}
+                    {learnMore && <a href={learnMore} style={{marginLeft:"50px" , textDecoration:"none"}}>Learn More →</a>}
                 </div>
                 
-                <div className='mt-4'>
-                    <a href={googlePlay}>
-                    <img src="media/images/googlePlayBadge.svg"  alt=""></img>
+                {/* Store Badges */}
+                <div className='mt-4 d-flex align-items-center'>
+                    <a href={googlePlay} target="_blank" rel="noopener noreferrer">
+                        <img src="media/images/googlePlayBadge.svg" alt="Google Play Store" />
                     </a>
-                    <a href={appStore}>
-                    <img src="media/images/appstoreBadge.svg"  alt=""></img>
-                   </a>
-                   </div>
-                
+                    <a href={appStore} target="_blank" rel="noopener noreferrer" style={{ marginLeft: "20px" }}>
+                        <img src="media/images/appstoreBadge.svg" alt="App Store" />
+                    </a>
+                </div>
             </div>
-
         </div>
       </div>
-     );
+    );
 }
 
 export default LeftSection;
